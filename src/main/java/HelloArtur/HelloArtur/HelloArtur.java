255cb7d84faa40982234610c9d3d05734786f2c7
artur102430 <48502972+artur102430@users.noreply.github.com>
2019-04-02 17:59:38 +0500

    Update and rename HelloArtur to HelloArtur.java

---
+++ src/main/java/HelloArtur/HelloArtur/HelloArtur.java
@@ -0,0 +1,14 @@
+package HelloArtur.HelloArtur;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloArtur
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Artur!" );
+    }
+}
+
